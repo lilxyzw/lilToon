8e9997fe72c861ce82bff57056567c6f27be6e0d
lilxyzw <lilxyzw@gmail.com>
2024-10-16 18:06:43 +0900

    chore: update version to 1.8.3

--- Assets/lilToon/Editor/lilConstants.cs
+++ Assets/lilToon/Editor/lilConstants.cs
@@ -6,7 +6,7 @@ namespace lilToon
 {
     public class lilConstants
     {
-        public const string currentVersionName = "1.8.2";
+        public const string currentVersionName = "1.8.3";
         public const int currentVersionValue = 44;
 
         internal const string boothURL = "https://lilxyzw.booth.pm/";
